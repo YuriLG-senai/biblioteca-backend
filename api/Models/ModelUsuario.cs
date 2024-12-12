aa2f98518206a504c3a1870e827707148b7fce15
unknown <yuril3300@gmail.com>
2024-12-12 08:46:35 -0300

    initial

---
+++ api/Models/ModelUsuario.cs
@@ -0,0 +1,10 @@
+namespace BookManagerAPI.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public long Telefone { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public int Idade { get; set; }
+    }
+}   
